94e4f2c7d4dc70e3df01dfe2bb739d50bf9669fb
fangngng <fangngng@163.com>
2015-08-02 08:25:52 +0800

    code

--- learn1.sql
+++
@@ -1,9 +0,0 @@
-
-select orderid,custid,empid,shipperid,orderdate,val,
-	ROW_NUMBER() over(partition by custid order by val) as rolNUM,
-	RANK() over(order by val) as rank1,
-	DENSE_RANK() over(order by val) as denserank,
-	Ntile(10) over(order by val) as ntile1
-from Sales.OrderValues
-order by custid asc , rolNUM asc
-
